00b5e43582434955452e58ec0a3527bdc21a7417
Kaawya <kaawyakavindee231@gmail.com>
2020-10-14 14:41:25 +0530

    first commit

---
+++ Question1.js
@@ -0,0 +1,22 @@
+//Print numbers from 1 to 100 to the console, but for multiples of three, print "Fizz", for multiples of five, print "Buzz" and for numbers which are both multiples of three and five, print "FizzBuzz".
+
+
+for ( var i = 1; i <= 100; i++ )
+{
+  if ( i%3 === 0 && i%5 === 0 )
+  {
+    console.log( i + " FizzBuzz" );
+  }
+  else if ( i%3 === 0 ) 
+  {
+    console.log(i+ " Fizz" );
+  }
+  else if ( i%5 === 0 ) 
+  {
+    console.log(i+ " Buzz" );
+  }
+  else
+  {
+    console.log(i);
+  }
+}
---
+++ Question2.js
@@ -0,0 +1,16 @@
+function foo(int n)
+{
+	console.log(n);
+	
+    if (n == 0)
+	{
+		return 0;
+	}
+	else if (n == 1) 
+	{
+		return 1;
+	}
+	else if (n > 1) {
+		return Fn - 1 + Fn + 2
+	}
+}
